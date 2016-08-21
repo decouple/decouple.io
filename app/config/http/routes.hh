11513f707079c2202cba918e95ae764374fe32fd
Andrew Ewing <drew@phenocode.com>
2016-08-21 05:55:04 +0000

    Improved configuration & routing

--- app/config/http/routes.hh
+++ app/config/http/routes.hh
@@ -1,4 +1,6 @@
 <?hh // partial
 use Decouple\Singleton\Router;
 Router::get("/", "FrontController@index");
-Router::restful("/news/(.*)", "NewsController");
+
+Router::restful("/news", "NewsController");
+Router::restful("/admin/news", "Admin\NewsController");
